0c77b4477a84b5707425977f3ea4802d3178983b
Martin <skydiver@users.noreply.github.com>
2017-02-21 16:38:47 -0300

    1.2.1: Fix when using title & URL with JavaScript

--- assets/js/ssbuttons.js
+++ assets/js/ssbuttons.js
@@ -3,6 +3,6 @@ var url   = document.location.href;
 var links = document.querySelectorAll('.SocialSharingButtons a');
 
 links.forEach(function(item) {
-    item.href = item.href.replace('___title___', encodeURI(title));
-    item.href = item.href.replace('___url___'  , encodeURI(url));
+    item.href = item.href.replace(/___title___/g, encodeURI(title));
+    item.href = item.href.replace(/___url___/g  , encodeURI(url));
 });
